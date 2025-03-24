dcd68b80fafd0b24a512351359314427256da20a
GitHub <rakshitbharat@gmail.com>
2025-03-25 00:23:26 +0530

    -

---
+++ src/__tests__/helloWorld.test.ts
@@ -0,0 +1,7 @@
+describe('Hello World Test', () => {
+  test('should return Hello World', () => {
+    const expected = 'Hello World';
+    const actual = 'Hello World';
+    expect(actual).toBe(expected);
+  });
+});
