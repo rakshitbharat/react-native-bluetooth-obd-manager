3a0050c43505b99cbac9b15ec54eb6395fcaa300
GitHub <rakshitbharat@gmail.com>
2025-04-05 15:46:36 +0000

    -

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
+// babel.config.js
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-react-jsx'],
   ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,35 @@
+// jest.config.js
 module.exports = {
-  preset: 'react-native',
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
-  silent: false,
-  testTimeout: 30000, // Increased timeout for async tests
-  transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|react-native-ble-manager)/)'
+  preset: 'react-native', // Use the standard preset for React Native
+  testEnvironment: 'node', // Usually correct for testing library logic without native UI
+  moduleFileExtensions: [
+      "ts",
+      "tsx",
+      "js",
+      "jsx",
+      "json",
+      "node"
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/__mocks__/'
+  modulePathIgnorePatterns: [
+      "<rootDir>/example/node_modules", // Ignore example app modules
+      "<rootDir>/lib/" // Ignore built output
   ],
-  collectCoverage: true,
-  bail: false // Don't stop on first test failure
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  // Ensure setup file runs AFTER environment is set up
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect', // Provides extra RN matchers
+    '<rootDir>/jest.setup.js', // Your custom setup file for mocks
+  ],
+  collectCoverageFrom: [
+      "src/**/*.{ts,tsx}", // Collect coverage from source files
+      "!src/types/**/*", // Exclude type definitions
+      "!src/index.ts" // Exclude main index export
+  ],
+  coverageReporters: [
+      "json-summary",
+      "text",
+      "lcov"
+  ],
 };
