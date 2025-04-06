29a2c46d0a84e7071feea4771e1c5ba9b7c5f125
GitHub <rakshitbharat@gmail.com>
2025-04-06 14:50:27 +0000

    -

--- eslint.config.js
+++ eslint.config.js
@@ -69,6 +69,9 @@ module.exports = [
       'react-native/no-single-element-style-arrays': 'warn',
     },
     settings: {
+      react: {
+        version: 'detect'
+      },
       'react-native/style-sheet-object-names': ['StyleSheet', 'ViewStyles', 'TextStyles']
     }
   },
