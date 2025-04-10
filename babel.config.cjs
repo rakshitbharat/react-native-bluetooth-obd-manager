e696ecd249877a6573bff2cb766cc39741b0edb9
GitHub <rakshitbharat@gmail.com>
2025-04-10 23:05:17 +0530

    -

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['@babel/plugin-proposal-export-namespace-from'],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    ['@babel/plugin-transform-runtime', { regenerator: true }]
+  ],
   env: {
+    production: {
+      plugins: ['transform-remove-console']
+    },
     test: {
       plugins: ['@babel/plugin-transform-runtime']
     }
